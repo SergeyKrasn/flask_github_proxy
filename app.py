36a0d4ad926c299cf4e9ca4d58bdf84c90ac1b92
SergeyKrasn <sergeyKrasn@gmail.com>
2021-10-04 08:23:18 +0200

    fix and clean

--- app.py
+++ app.py
@@ -1,117 +1,78 @@
-from flask import Flask
-import datetime
-
 from flask import Flask, jsonify
 from github import Github
 import requests
 import logging
-import json
+import os
+import sys
+import datetime
 
-TOKEN = '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'
-# USER = 'pallets'
-# REPO = 'flask'
+TOKEN = os.environ.get('TOKEN')
 
-# USER = 'temoto'
-# REPO = 'vender'
+# REPO = 'pallets/flask'
+# REPO = 'temoto/vender'
+# REPO = 'PyGithub/PyGithub'
+REPO = os.environ.get('REPO')
 
-USER = 'PyGithub'
-REPO = 'PyGithub'
-
-
+# print(sys.argv)
 app = Flask(__name__)
 
+def get_repo(token, repo):
+    github = Github(token)
+    return github.get_repo(repo)
 
-@app.route('/hello', methods=["GET"])
-def hello_world():  # put application's code here
-    return jsonify({'hello': 'Hello World!'})
-
-@app.route('/flask', methods=["GET"])
-def github_pallets_flask_details():
-    g = Github(TOKEN)
-    f = g.get_user('pallets').get_repo('flask')
-    return jsonify({'id': f.id, 'full_name': f.full_name})
-
-@app.route('/flask/full_name', methods=["GET"])
-def github_pallets_flask_full_name():
-    g = Github(TOKEN)
-    f = g.get_user('pallets').get_repo('flask')
-    return f.full_name
-
-@app.route('/flask/issues_url', methods=["GET"])
-def github_pallets_flask_issues_url():
-    g = Github(TOKEN)
-    f = g.get_user('pallets').get_repo('flask')
-    return f.get_issue()
-
-@app.route('/flask/issues', methods=["GET"])
-def github_pallets_flask_issues():
-    g = Github(TOKEN)
-    iss = g.get_user('pallets').get_repo('flask').get_issues()
-
-    return jsonify({'issues_count': iss.totalCount})
-
-@app.route('/flask/pulls', methods=["GET"])
-def github_pallets_flask_pulls():
-    g = Github(TOKEN)
-    pulls = g.get_user('pallets').get_repo('flask').get_pulls()
-
-    return jsonify({'pulls_count': pulls.totalCount})
-
-@app.route('/flask/pulls_14days', methods=["GET"])
-def github_pallets_flask_pulls_14days():
-    g = Github(TOKEN)
-    # aSince = (datetime.datetime.now() - datetime.timedelta(days=14))
-    pulls = g.get_user('pallets').get_repo('flask').get_pulls()
-    count = 0
-    for pull in pulls:
-        print(f"merged_at: {pull.merged_at}")
-        # if pull.merged_at is None and (datetime.datetime.today() - pull.merged_at) > datetime.timedelta(days=14):
-        if pull.merged_at is None and (datetime.datetime.today() - pull.created_at) > datetime.timedelta(days=14):
-                count += 1
-
-    return jsonify({'pulls_count': count})
-
-@app.route('/PyGithub/pulls', methods=["GET"])
-def github_PyGithub_PyGithub_pulls():
-    g = Github(TOKEN)
-    pulls = g.get_user('PyGithub').get_repo('PyGithub').get_pulls()
-
-    return jsonify({'pulls_count': pulls.totalCount})
-
-@app.route('/PyGithub/pulls_14days', methods=["GET"])
-def github_PyGithub_PyGithub_pulls_14days():
-    g = Github(TOKEN)
-    # aSince = (datetime.datetime.now() - datetime.timedelta(days=14))
-    pulls = g.get_user('PyGithub').get_repo('PyGithub').get_pulls()
-    count = 0
-    for pull in pulls:
-        print(f"merged_at: {pull.merged_at}")
-        # if pull.merged_at is None and (datetime.datetime.today() - pull.merged_at) > datetime.timedelta(days=14):
-        if pull.merged_at is None and (datetime.datetime.today() - pull.created_at) > datetime.timedelta(days=14):
-                count += 1
-
-    return jsonify({'pulls_count': count})
-
-@app.route('/details', methods=["GET"])
+@app.route('/details')
 def get_details():
-    res = requests.request('GET', f'https://api.github.com/repos/{USER}/{REPO}')
-    return res.content
+    # res = requests.request('GET', f'https://api.github.com/repos/{REPO}')
+    # return res.content
+    r = get_repo(TOKEN, REPO)
+    return r.raw_data
 
-@app.route('/issues', methods=["GET"])
+@app.route('/issues')
 def get_issues():
-    res = requests.request('GET', f'https://api.github.com/repos/{USER}/{REPO}/issues')
+    res = requests.get(f'https://api.github.com/repos/{REPO}/issues')
     return res.content
+    # r = get_repo(TOKEN, REPO)
+    # issues = r.get_issues()
+    # res = []
+    # for iss in issues:
+    #     res.append(iss.raw_data)
+    # return jsonify(res)
 
-@app.route('/forks', methods=["GET"])
+@app.route('/forks')
 def get_forks():
-    res = requests.request('GET', f'https://api.github.com/repos/{USER}/{REPO}/forks')
+    res = requests.request('GET', f'https://api.github.com/repos/{REPO}/forks')
     return res.content
 
-@app.route('/pulls', methods=["GET"])
+
+@app.route('/pulls')
 def get_pulls():
-    res = requests.request('GET', f'https://api.github.com/repos/{USER}/{REPO}/pulls')
+    res = requests.request('GET', f'https://api.github.com/repos/{REPO}/pulls')
     return res.content
+
+@app.route('/pulls/14days')
+def pulls_14days():
+    r = get_repo(TOKEN, REPO)
+    pulls = r.get_pulls()
+    res = []
+    for pull in pulls:
+        if pull.merged_at is None and (datetime.datetime.today() - pull.created_at) > datetime.timedelta(days=14):
+            res.append(pull.raw_data)
+
+    return jsonify(res)
 
 
 if __name__ == '__main__':
+    print('start app.py')
+
+    args = sys.argv[1:]
+    if len(args) < 2:
+        print("""\
+        USAGE: python app.py ????? 
+        """)
+        sys.exit(1)
+
+    # app.config.update({
+    #     'REPO': REPO,
+    #     'TOKEN': TOKEN
+    # })
     app.run()
